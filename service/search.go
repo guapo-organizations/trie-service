db60ac59314f29f20cbce3a7304dbe7a79445c3c
liangyu <liangyu@22.cn>
2019-06-19 14:11:39 +0800

    rpc服务注册

---
+++ service/search.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"context"
+	"github.com/guapo-organizations/trie-service/proto/trie"
+)
+
+//关键词搜索
+func (this *TrieService) KeySearch(ctx context.Context, request *trie.KeySearchTrieRequest) (*trie.KeySearchTrieResponse, error) {
+	return nil, nil
+}
